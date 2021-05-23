931f5e1fc8aa420666eba7c060b87afbb3d1dfcb
João Luiz Silva <joaoluizsilva.jls@gmail.com>
2021-05-23 19:10:58 -0300

    Uso de var e let

--- src/js/meusscripts.js
+++ src/js/meusscripts.js
@@ -1,1 +1,16 @@
 console.log("Curso de JavaScript por João Luiz");
+window.alert("Olá Mundo!!");
+window.alert("Seja bem vindo o Js");
+
+let like =  window.confirm("Esta gostando de JS?");
+console.log(like); 
+let campo = window.prompt("Qual seu nome???");
+console.log(campo);
+
+let n1 = 8.5;
+let n2 = 5;
+let n3 = 15;
+
+let s1 = "João Luiz, aspa dupla";
+let s2 = 'Curso em Video, aspa simple';
+let s3 = `Developer, com crase`;

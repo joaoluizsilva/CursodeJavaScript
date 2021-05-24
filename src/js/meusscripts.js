31dd1d1fbc7a0ffdb55c34fd9b8ab3bd268f28ca
João Luiz Silva <joaoluizsilva.jls@gmail.com>
2021-05-23 23:16:48 -0300

    Usando o parseInt()

--- src/js/meusscripts.js
+++ src/js/meusscripts.js
@@ -2,10 +2,23 @@ console.log("Curso de JavaScript por João Luiz");
 window.alert("Olá Mundo!!");
 window.alert("Seja bem vindo o Js");
 
-let like =  window.confirm("Esta gostando de JS?");
+var like =  window.confirm("Esta gostando de JS?");
 console.log(like); 
-let campo = window.prompt("Qual seu nome???");
-console.log(campo);
+var nome = window.prompt("Qual seu nome???");
+console.log(nome);
+window.alert("Seja vem vindo, " + nome + "!");
+
+var numero1 = Number.parseInt(window.prompt("Informe o valor 1!"));
+var numero2 = Number.parseInt(window.prompt("Informe o valor 2"));
+var res = numero1 + numero2;
+alert("O valor é de : "+ res);
+
+
+if (like == true) {
+    alert("Verdadeiro")
+} else {
+    alert("Falso")
+}
 
 let n1 = 8.5;
 let n2 = 5;
